3d1018bfdf4ec5d07fca8ab20ad396135074458e
JC <29726242+jc211@users.noreply.github.com>
2022-06-29 18:44:05 +1000

    Add render groundtruth depth option

--- src/nerf_loader.cu
+++ src/nerf_loader.cu
@@ -619,7 +619,7 @@ NerfDataset load_nerf(const std::vector<filesystem::path>& jsonpaths, float shar
 					if (wa != dst.res.x() || ha != dst.res.y()) {
 						throw std::runtime_error{std::string{"Depth image has wrong resolution: "} + depthpath.str()};
 					}
-					//tlog::success() << "Depth loaded from " << depthpath;
+					tlog::success() << "Depth loaded from " << depthpath;
 				}
 			}
 
