ba68e0f318a437848b58107b7b3867e161d98726
Andrew Guo <andrewg@nvidia.com>
2023-06-30 22:09:33 -0400

    Add argument to specify marching cubes density threshold

--- scripts/run.py
+++ scripts/run.py
@@ -56,6 +56,7 @@ def parse_args():
 
 	parser.add_argument("--save_mesh", default="", help="Output a marching-cubes based mesh from the NeRF or SDF model. Supports OBJ and PLY format.")
 	parser.add_argument("--marching_cubes_res", default=256, type=int, help="Sets the resolution for the marching cubes grid.")
+	parser.add_argument("--marching_cubes_density_thresh", default=2.5, type=float, help="Sets the density threshold for marching cubes.")
 
 	parser.add_argument("--width", "--screenshot_w", type=int, default=0, help="Resolution width of GUI and screenshots.")
 	parser.add_argument("--height", "--screenshot_h", type=int, default=0, help="Resolution height of GUI and screenshots.")
@@ -267,8 +268,9 @@ if __name__ == "__main__":
 
 	if args.save_mesh:
 		res = args.marching_cubes_res or 256
-		print(f"Generating mesh via marching cubes and saving to {args.save_mesh}. Resolution=[{res},{res},{res}]")
-		testbed.compute_and_save_marching_cubes_mesh(args.save_mesh, [res, res, res])
+		thresh = args.marching_cubes_density_thresh or 2.5
+		print(f"Generating mesh via marching cubes and saving to {args.save_mesh}. Resolution=[{res},{res},{res}], Density Threshold={thresh}")
+		testbed.compute_and_save_marching_cubes_mesh(args.save_mesh, [res, res, res], thresh=thresh)
 
 	if ref_transforms:
 		testbed.fov_axis = 0
